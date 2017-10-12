7b2d9fbb8267aad3da2d7cdc76f016d08e9c10e1
Rodrigo <maraxus@localhost.localdomain>
2017-10-11 23:23:34 -0300

    Post Index route is functional now

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //pass a formated, pretty created date
+    public function created_time() {
+    	return $this->created_at;
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,5 @@
 <div class="blog-post">
   <h2 class="blog-post-title">{{ $post->title }}</h2>
-  <p class="blog-post-meta">{{ $post->created() }} by <a href="#">{{ $post->author }}</a></p>
+  <p class="blog-post-meta">{{ $post->created_time() }} by <a href="#">{{ $post->author }}</a></p>
   {{ $post->body }}
 </div><!-- /.blog-post -->
